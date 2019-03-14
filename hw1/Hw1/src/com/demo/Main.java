a2ccd4a8cbaef609570a8ea3682145ac86e63009
novayo <f14051172@gs.ncku.edu.tw>
2019-03-14 22:42:40 +0800

    Upload for new input format hw1

--- hw1/Hw1/src/com/demo/Main.java
+++ hw1/Hw1/src/com/demo/Main.java
@@ -4,22 +4,21 @@ public class Main {
 
 	public static void main(String[] args) {
 		String answerString = "";
-		String[] partOfArgs = args[0].split(" ");
 		
-		for (int i=0; i<partOfArgs.length; i++) {
+		for (int i=0; i<args.length; i++) {
 			// Do <Pig Latin rule>
-			if (partOfArgs[i].charAt(0) == 'a' || partOfArgs[i].charAt(0) == 'e' || partOfArgs[i].charAt(0) == 'i' || 
-				partOfArgs[i].charAt(0) == 'o' || partOfArgs[i].charAt(0) == 'u') {
-				partOfArgs[i] = partOfArgs[i] + "ay";
+			if (args[i].charAt(0) == 'a' || args[i].charAt(0) == 'e' || args[i].charAt(0) == 'i' || 
+				args[i].charAt(0) == 'o' || args[i].charAt(0) == 'u') {
+				args[i] = args[i] + "ay";
 			} else {
-				partOfArgs[i] = partOfArgs[i].substring(1) + partOfArgs[i].charAt(0) + "ay";
+				args[i] = args[i].substring(1) + args[i].charAt(0) + "ay";
 			}
 			
 			// Upper case the capital
-			partOfArgs[i] = (char)(partOfArgs[i].charAt(0)-32) + partOfArgs[i].substring(1);
+			args[i] = (char)(args[i].charAt(0)-32) + args[i].substring(1);
 			
 			// Add to answerString
-			answerString += (partOfArgs[i] + ' ');
+			answerString += (args[i] + ' ');
 		}
 		
 		System.out.print(answerString.trim()); // Get rid of the last space	
