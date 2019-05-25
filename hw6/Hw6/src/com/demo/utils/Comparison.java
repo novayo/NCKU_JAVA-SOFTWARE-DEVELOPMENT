4fa1ed7acd8a7ed952b08e6861aa6eec5ff1bdbe
novayo <f14051172@gs.ncku.edu.tw>
2019-05-25 18:14:49 +0800

    Upload Final Project
    	* Unfinished

--- hw6/Hw6/src/com/demo/utils/Comparison.java
+++ hw6/Hw6/src/com/demo/utils/Comparison.java
@@ -3,9 +3,17 @@ package com.demo.utils;
 public class Comparison implements IOperation{
 	
 	public String perform(String num1, String num2) {
+		
 		if (num1.charAt(0) != '-' && num2.charAt(0) != '-') {
-			Subtraction utilSubtraction = new Subtraction();
-			return utilSubtraction.perform(num1, num2);
+			
+			
+			//Subtraction.perform(num1, num2);
+			
+			Subtraction obj = new Subtraction();
+			return obj.perform(num1, num2);
+			
+			
+			
 		} else if (num1.charAt(0) != '-' && num2.charAt(0) == '-') {
 			return "1";
 		} else if (num1.charAt(0) == '-' && num2.charAt(0) != '-') {
